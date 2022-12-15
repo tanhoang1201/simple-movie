57de9346eaf29f62757c56f82a97517d3a0b2222
tanhoang <htan09817@gmail.com>
2022-12-15 10:13:52 +0700

    ok3

--- src/layouts/homeComponents/MovieList.jsx
+++ src/layouts/homeComponents/MovieList.jsx
@@ -1,109 +1,110 @@
-import PropTypes from "prop-types";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { FreeMode, Pagination } from "swiper";
-import "swiper/css/pagination";
-import "swiper/css";
+import PropTypes from 'prop-types'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { FreeMode, Scrollbar } from 'swiper'
+import 'swiper/css/scrollbar'
+import 'swiper/css'
 
-import MovieCart from "../../components/MovieCart";
+import MovieCart from '../../components/MovieCart'
 
 function MovieList({ className, title }) {
-	return (
-		<section className={className}>
-			<h2 className="text-2xl font-semibold mb-10">{title}</h2>
-			<Swiper
-				slidesPerView={4}
-				spaceBetween={30}
-				grabCursor={true}
-				className="mySwiper"
-				freeMode={true}
-				pagination={{
-					clickable: true,
-				}}
-				modules={[FreeMode, Pagination]}
-			>
-				<SwiperSlide>
-					<MovieCart />
-				</SwiperSlide>
-				<SwiperSlide>
-					<MovieCart />
-				</SwiperSlide>
-				<SwiperSlide>
-					<MovieCart />
-				</SwiperSlide>
-				<SwiperSlide>
-					<MovieCart />
-				</SwiperSlide>
-				<SwiperSlide>
-					<MovieCart />
-				</SwiperSlide>
-				<SwiperSlide>
-					<MovieCart />
-				</SwiperSlide>
-				<SwiperSlide>
-					<MovieCart />
-				</SwiperSlide>
-				<SwiperSlide>
-					<MovieCart />
-				</SwiperSlide>
-				<SwiperSlide>
-					<MovieCart />
-				</SwiperSlide>
-				<SwiperSlide>
-					<MovieCart />
-				</SwiperSlide>
-				<SwiperSlide>
-					<MovieCart />
-				</SwiperSlide>
-				<SwiperSlide>
-					<MovieCart />
-				</SwiperSlide>
-				<SwiperSlide>
-					<MovieCart />
-				</SwiperSlide>
-				<SwiperSlide>
-					<MovieCart />
-				</SwiperSlide>
-				<SwiperSlide>
-					<MovieCart />
-				</SwiperSlide>
-				<SwiperSlide>
-					<MovieCart />
-				</SwiperSlide>
-				<SwiperSlide>
-					<MovieCart />
-				</SwiperSlide>
-				<SwiperSlide>
-					<MovieCart />
-				</SwiperSlide>
-				<SwiperSlide>
-					<MovieCart />
-				</SwiperSlide>
-				<SwiperSlide>
-					<MovieCart />
-				</SwiperSlide>
-				<SwiperSlide>
-					<MovieCart />
-				</SwiperSlide>
-				<SwiperSlide>
-					<MovieCart />
-				</SwiperSlide>
-				<SwiperSlide>
-					<MovieCart />
-				</SwiperSlide>
-				<SwiperSlide>
-					<MovieCart />
-				</SwiperSlide>
-				<SwiperSlide>
-					<MovieCart />
-				</SwiperSlide>
-			</Swiper>
-		</section>
-	);
+    return (
+        <section className={className}>
+            <h2 className='text-2xl font-semibold mb-10'>{title}</h2>
+            <Swiper
+                slidesPerView={4}
+                spaceBetween={30}
+                grabCursor={true}
+                slidesPerGroup={2}
+                className='mySwiper pb-8'
+                scrollbar={{
+                    hide: false,
+                    draggable: true,
+                }}
+                modules={[Scrollbar]}
+            >
+                <SwiperSlide>
+                    <MovieCart />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MovieCart />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MovieCart />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MovieCart />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MovieCart />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MovieCart />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MovieCart />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MovieCart />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MovieCart />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MovieCart />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MovieCart />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MovieCart />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MovieCart />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MovieCart />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MovieCart />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MovieCart />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MovieCart />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MovieCart />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MovieCart />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MovieCart />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MovieCart />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MovieCart />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MovieCart />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MovieCart />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <MovieCart />
+                </SwiperSlide>
+            </Swiper>
+        </section>
+    )
 }
 
 MovieList.propTypes = {
-	className: PropTypes.string,
-	title: PropTypes.string.isRequired,
-};
+    className: PropTypes.string,
+    title: PropTypes.string.isRequired,
+}
 
-export default MovieList;
+export default MovieList
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{js,jsx,ts,tsx}"],
-	theme: {
-		fontFamily: {
-			body: ["DM Sans", "sans-serif"],
-		},
-		extend: {
-			colors: {
-				primary: "#f62682",
-			},
-		},
-	},
-	plugins: [],
-};
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        fontFamily: {
+            body: ['DM Sans', 'sans-serif'],
+        },
+        extend: {
+            colors: {
+                primary: '#f62682',
+            },
+        },
+    },
+    plugins: [],
+}
