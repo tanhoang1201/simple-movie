2592a455b682a0d959958c5c82b3253d3e360253
tanhoang <htan09817@gmail.com>
2022-12-15 07:31:54 +0700

    ok2

--- src/layouts/homeComponents/MovieList.jsx
+++ src/layouts/homeComponents/MovieList.jsx
@@ -1,21 +1,109 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Pagination } from "swiper";
+import "swiper/css/pagination";
+import "swiper/css";
 
-import MovieCart from '../../components/MovieCart'
+import MovieCart from "../../components/MovieCart";
 
 function MovieList({ className, title }) {
-    return (
-        <section className={className}>
-            <h2 className='text-2xl font-semibold mb-10'>{title}</h2>
-            <div className='grid grid-cols-4'>
-                <MovieCart />
-            </div>
-        </section>
-    )
+	return (
+		<section className={className}>
+			<h2 className="text-2xl font-semibold mb-10">{title}</h2>
+			<Swiper
+				slidesPerView={4}
+				spaceBetween={30}
+				grabCursor={true}
+				className="mySwiper"
+				freeMode={true}
+				pagination={{
+					clickable: true,
+				}}
+				modules={[FreeMode, Pagination]}
+			>
+				<SwiperSlide>
+					<MovieCart />
+				</SwiperSlide>
+				<SwiperSlide>
+					<MovieCart />
+				</SwiperSlide>
+				<SwiperSlide>
+					<MovieCart />
+				</SwiperSlide>
+				<SwiperSlide>
+					<MovieCart />
+				</SwiperSlide>
+				<SwiperSlide>
+					<MovieCart />
+				</SwiperSlide>
+				<SwiperSlide>
+					<MovieCart />
+				</SwiperSlide>
+				<SwiperSlide>
+					<MovieCart />
+				</SwiperSlide>
+				<SwiperSlide>
+					<MovieCart />
+				</SwiperSlide>
+				<SwiperSlide>
+					<MovieCart />
+				</SwiperSlide>
+				<SwiperSlide>
+					<MovieCart />
+				</SwiperSlide>
+				<SwiperSlide>
+					<MovieCart />
+				</SwiperSlide>
+				<SwiperSlide>
+					<MovieCart />
+				</SwiperSlide>
+				<SwiperSlide>
+					<MovieCart />
+				</SwiperSlide>
+				<SwiperSlide>
+					<MovieCart />
+				</SwiperSlide>
+				<SwiperSlide>
+					<MovieCart />
+				</SwiperSlide>
+				<SwiperSlide>
+					<MovieCart />
+				</SwiperSlide>
+				<SwiperSlide>
+					<MovieCart />
+				</SwiperSlide>
+				<SwiperSlide>
+					<MovieCart />
+				</SwiperSlide>
+				<SwiperSlide>
+					<MovieCart />
+				</SwiperSlide>
+				<SwiperSlide>
+					<MovieCart />
+				</SwiperSlide>
+				<SwiperSlide>
+					<MovieCart />
+				</SwiperSlide>
+				<SwiperSlide>
+					<MovieCart />
+				</SwiperSlide>
+				<SwiperSlide>
+					<MovieCart />
+				</SwiperSlide>
+				<SwiperSlide>
+					<MovieCart />
+				</SwiperSlide>
+				<SwiperSlide>
+					<MovieCart />
+				</SwiperSlide>
+			</Swiper>
+		</section>
+	);
 }
 
 MovieList.propTypes = {
-    className: PropTypes.string,
-    title: PropTypes.string.isRequired,
-}
+	className: PropTypes.string,
+	title: PropTypes.string.isRequired,
+};
 
-export default MovieList
+export default MovieList;
